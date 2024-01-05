aa33ff2e2a641bf287a74114717c247fa1e04924
tadghh <thenry.he@gmail.com>
2024-01-05 01:14:23 -0600

    Models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_04_234706) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_05_062855) do
+  create_table "submissions", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "url"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_submissions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +33,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_04_234706) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "submissions", "users"
 end
