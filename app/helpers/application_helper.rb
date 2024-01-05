0d31617f548e5ea0c65d1e3dc495951fb01f4c2a
tadghh <thenry.he@gmail.com>
2024-01-05 17:10:58 -0600

    linting and tailwind fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
-module ApplicationHelper
+# frozen_string_literal: true # rubocop:disable Layout/EndOfLine
+
+module ApplicationHelper # rubocop:disable Style/Documentation
+  def render_svg(name, options = {})
+    options[:title] ||= name.underscore.humanize
+    options[:aria] = true
+    options[:nocomment] = true
+    options[:class] = options.fetch(:styles, 'fill-current text-gray-500')
+    filename = "#{name}.svg"
+    inline_svg_tag(filename, options)
+  end
+
+  def nav
+    render 'partials/nav'
+  end
 end
