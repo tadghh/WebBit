79cec95314b32204d29007dbbc8eeaa297904fd4
tadghh <thenry.he@gmail.com>
2024-01-14 20:40:33 -0600

    comments layout

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,40 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_comment, only: %i[edit update destroy show]
+  before_action :set_submission
+
+  def new; end
+
+  def create
+    @comment = @submission.comments.new(comment_params)
+    @comment.user = current_user
+    respond_to do |format|
+      format.turbo_stream
+      if @comment.save
+        format.html { redirect_to submission_path(@submission), notice: 'Comment posted successfully' }
+      else
+        format.html { redirect_to submission_path(@submission), alert: 'Comment could not be created' }
+      end
+    end
+  end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  def set_submission
+    @submission = Submission.find(params[:submission_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:reply, :submission_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :communities
-  resources :submissions
+  resources :submissions do
+    resources :comments
+  end
   devise_for :users
   # get 'home/index'
   # # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
