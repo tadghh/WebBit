f5ec54409281ea84a178aa2e22d9b090c56e9ca3
tadghh <thenry.he@gmail.com>
2024-01-22 03:38:30 -0600

    submissions controller docs

--- app/controllers/admin/submissions_controller.rb
+++ app/controllers/admin/submissions_controller.rb
@@ -1,5 +1,10 @@
-class Admin::SubmissionsController < ApplicationController
-  def index
-    @submissions = Submissions.all.sort
+# frozen_string_literal: true
+
+module Admin
+  # Returns all submissions, sorted in ascending order.
+  class SubmissionsController < ApplicationController
+    def index
+      @submissions = Submissions.all.sort
+    end
   end
 end
