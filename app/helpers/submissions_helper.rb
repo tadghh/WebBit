b7840bb9ca34da4eeee5efd66724b20f60d8b1c6
tadghh <thenry.he@gmail.com>
2024-01-22 00:23:56 -0600

    small changes

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,6 +1,7 @@
 module SubmissionsHelper
   def inject_feed_ad(index)
     return if user_signed_in? && current_user.subscribed?
+
     return unless ((index + 1) % 3).zero?
 
     render partial: 'feed_ad'
