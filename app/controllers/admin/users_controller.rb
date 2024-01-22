9987513b7a4d1b7f1006502a263b059ac6c2acc2
tadghh <thenry.he@gmail.com>
2024-01-22 03:39:39 -0600

    users controller docs

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,10 @@
-class Admin::UsersController < ApplicationController
-  def index
-    @users = User.all.sort
+# frozen_string_literal: true
+
+module Admin
+  # Returns all users in ascending order
+  class UsersController < ApplicationController
+    def index
+      @users = User.all.sort
+    end
   end
 end
