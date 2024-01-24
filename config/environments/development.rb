2c769ac21fcf6792f05ecb94a19239ec7d5081c0
tadghh <thenry.he@gmail.com>
2024-01-24 01:14:50 -0600

    changed default callback behaviour

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,7 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Raise error when a before_action's only/except options reference missing actions
-  config.action_controller.raise_on_missing_callback_actions = true
+  config.action_controller.raise_on_missing_callback_actions = false
   # config.action_controller.delivery_method = :letter_opener
   # config.action_controller.perform_deliveries = true
 end
